41e625dfc09ee7bd63075b9d6734416282cfd0c5
li1ht2ay <li1ht2ay@users.noreply.github.com>
2024-03-15 07:49:11 +0000

    Create a.yml

--- build-rom.sh
+++ build-rom.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+<<<<<<< HEAD
 IMAGE_TAG="andwn/ym2017:latest"
+=======
+IMAGE_TAG="ym1202173-ca4sk5ll/ym1202173-ca4sk5ll:test1"
+>>>>>>> 0c077de (Create build-rom.sh)
 IMAGE_NAME="ym2017"
 MARSDEV_REVISION="2017.09.12"
 
